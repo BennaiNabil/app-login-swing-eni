51c118dae9abc9094fabeca86ce0911457cf1b81
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-01-15 09:15:34 +0100

    Modification de l'appel dans l'AppLogin: utilisation d'un lambda référencé

--- src/fr/loginapp/ihm/AppLogin.java
+++ src/fr/loginapp/ihm/AppLogin.java
@@ -5,11 +5,6 @@ import javax.swing.*;
 public class AppLogin {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new EcranLogin();
-            }
-        });
+        SwingUtilities.invokeLater(EcranLogin::new);
     }
 }
